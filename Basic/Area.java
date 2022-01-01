e2dbfd65382e98547c4b43cbc1835adfce65f5f1
Aditya <81923498+Adityaraj-30@users.noreply.github.com>
2022-01-01 19:02:08 +0530

    Add files via upload

---
+++ Basic/Area.java
@@ -0,0 +1,11 @@
+package JAVA.Basic;
+
+public class Area {
+    public static void main(String[] args) {
+        double pi, r, area;
+        r = 10.8; // radius of circle
+        pi = 3.1416; // pi, approximately
+        area = pi * r * r; // compute area
+        System.out.println("Area of circle is " + area);
+    }
+}
